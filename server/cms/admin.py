94f179cbee024a11f8e39d6c42998e3a3c667327
Виноградов Константин <kostya.06.05.1995@gmail.com>
2021-10-31 18:44:06 +0300

    Обновил модели и фикстуры и представления в админке

--- server/cms/admin.py
+++ server/cms/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
-from .models import TPTypeWork, Okolotok, Station, ReportOfWork, PreReportOfWork, ProfileUser
+from django.db.models.aggregates import Count
 
+from .models import TechCard, Okolotok, Station, ReportOfWork, UserProfile, DeviceForWork
+from .forms import OkolotokAdminForm
 
 ###########################################################
 # Вспомогательные классы для отображения                  #
 ###########################################################
 
-# Вообще он мне не нужен
-class ProfileUserInline(admin.TabularInline):
+
+class UserProfileInline(admin.TabularInline):
     """Отображение пользователей в виде таблицы"""
-    model = ProfileUser
+    model = UserProfile
     extra = 0
-    readonly_fields = ['__str__']
+    readonly_fields = ('__str__',)
 
 
 # Вообще он мне не нужен
@@ -19,7 +21,7 @@ class ReportOfWorkInline(admin.TabularInline):
     """Отображение работ в виде таблицы"""
     model = ReportOfWork
     extra = 0
-    readonly_fields = ['__str__']
+    readonly_fields = ('__str__',)
 
 
 ###########################################################
@@ -28,46 +30,113 @@ class ReportOfWorkInline(admin.TabularInline):
 
 @admin.register(Okolotok)
 class OkolotokAdmin(admin.ModelAdmin):
-    list_display = ('name', 'id')
-    fields = ('name', 'id')
-    readonly_fields = ['id']
+    list_display = ('name', 'count_userprofile', 'count_report_of_work', 'id',)
+    fields = ('id', 'name', 'count_userprofile', 'count_report_of_work',)
+    readonly_fields = ('count_userprofile', 'count_report_of_work', 'id',)
+    search_fields = ('name', 'count_userprofile', 'count_report_of_work',)
+    ordering = ('id',)
 
+    def get_queryset(self, request):
+        qs = super(OkolotokAdmin, self).get_queryset(request)
+        qs = qs.annotate(count_userprofile=Count('userprofile')).order_by('-count_userprofile')
+        qs = qs.annotate(count_report_of_work=Count('report_of_work')).order_by('-count_report_of_work')
+        return qs
 
-@admin.register(ProfileUser)
-class ProfileUserAdmin(admin.ModelAdmin):
-    list_display = ('user_site', 'okolotok', 'active', 'id')
-    fields = ('user_site', 'okolotok', 'active', 'id')
-    readonly_fields = ['id']
-
-
-@admin.register(TPTypeWork)
-class TPTypeWorkAdmin(admin.ModelAdmin):
-    list_display = ('code', 'name', 'du46', 'id')
-    ordering = ('code',)
-    search_fields = ('code', 'name',)
-    list_filter = ('du46',)
-    fields = ('code', 'name','du46', 'id')
-    readonly_fields = ['id']
+    def count_userprofile(self, instance):
+        return instance.count_userprofile
     
+    count_userprofile.short_description = 'Работников'
+    count_userprofile.admin_order_field = 'count_userprofile'
+
+    def count_report_of_work(self, instance):
+        return instance.count_report_of_work
+
+    count_report_of_work.short_description = 'Выполненных техпроцессов'
+    count_report_of_work.admin_order_field = 'count_report_of_work'
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'okolotok', 'count_report_of_work', 'id',)
+    fields = ('user', 'okolotok', 'count_report_of_work', 'id',)
+    readonly_fields = ('id', 'count_report_of_work',)
+    search_fields = ('user__username', 'okolotok__name',)
+
+    # This will help you to disbale add functionality
+    def has_add_permission(self, request):
+        return False
+
+    # This will help you to disable delete functionaliyt
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_queryset(self, request):
+        qs = super(UserProfileAdmin, self).get_queryset(request)
+        qs = qs.annotate(count_report_of_work=Count('report_of_work')).order_by('-count_report_of_work')
+        return qs
+
+    def count_report_of_work(self, instance):
+        return instance.count_report_of_work
+
+    count_report_of_work.short_description = 'Выполненных техпроцессов'
+    count_report_of_work.admin_order_field = 'count_report_of_work'
+
+
+@admin.register(DeviceForWork)
+class DeviceForWorkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'count_tech_card', 'id')
+    fields = ('name', 'description', 'count_tech_card', 'id')
+    ordering = ('id',)
+    search_fields = ('name',)
+    readonly_fields = ('count_tech_card', 'id',)
+
+    def get_queryset(self, request):
+        qs = super(DeviceForWorkAdmin, self).get_queryset(request)
+        qs = qs.annotate(count_tech_card=Count('tech_card')).order_by('-count_tech_card')
+        return qs
+
+    def count_tech_card(self, instance):
+        return instance.count_tech_card
+
+    count_tech_card.short_description = 'Количество техкарт'
+    count_tech_card.admin_order_field = 'count_tech_card'
+
+
+@admin.register(TechCard)
+class TechCardAdmin(admin.ModelAdmin):
+    list_display_links = ('code', 'name',)
+    list_display = ('code', 'name', 'du46', 'order', 'id')
+    fields = ('code', 'name', 'description', 'device_for_work', 'du46', 'order', 'id')
+    ordering = ('code',)
+    search_fields = ('code', 'name', 'description')
+    list_filter = ('du46', 'order', 'device_for_work',)
+    filter_horizontal = ('device_for_work',)
+    readonly_fields = ['id']
+
 
 @admin.register(Station)
 class StationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'short_name', 'id')
-    fields = ('name', 'short_name', 'id')
-    readonly_fields = ['id']
+    list_display = ('name', 'short_name', 'count_report_of_work', 'id',)
+    fields = ('name', 'short_name', 'count_report_of_work', 'id',)
+    search_fields = ('name', 'short_name',)
+    readonly_fields = ('count_report_of_work', 'id',)
+
+    def get_queryset(self, request):
+        qs = super(StationAdmin , self).get_queryset(request)
+        qs = qs.annotate(count_report_of_work=Count('report_of_work')).order_by('-count_report_of_work')
+        return qs
+
+    def count_report_of_work(self, instance):
+        return instance.count_report_of_work
+
+    count_report_of_work.short_description = 'Выполненных техпроцессов'
+    count_report_of_work.admin_order_field = 'count_report_of_work'
 
 
 @admin.register(ReportOfWork)
 class ReportOfWorkAdmin(admin.ModelAdmin):
-    list_display = ('data_start', 'data_end', 'station', 'profile_user', 'note', 'subdivision', 'id')
-    fields = ('data_start', 'data_end', 'station', 'note', 'profile_user', 'subdivision', 'id', 'type_work')
-    filter_horizontal = ('type_work',)
-    readonly_fields = ['id']
+    list_display = ('date_start', 'date_end', 'station', 'okolotok', 'note', 'subdivision', 'id')
+    fields = ('date_start', 'date_end', 'station', 'note', 'userprofile', 'okolotok', 'subdivision', 'id', 'type_work')
+    filter_horizontal = ('type_work', 'userprofile',)
+    readonly_fields = ('id',)
+    ordering = ('date_start', 'date_end')
 
-
-@admin.register(PreReportOfWork)
-class PreReportOfWorkAdmin(admin.ModelAdmin):
-    list_display = ('id', 'data_start', 'data_end', 'status_run', 'station', 'profile_user', 'note', 'subdivision')
-    fields = ('data_start', 'data_end', 'status_run', 'station', 'note', 'profile_user', 'subdivision', 'id', 'type_work')
-    filter_horizontal = ('type_work',)
-    readonly_fields = ['id']
--- server/cms/models.py
+++ server/cms/models.py
@@ -1,43 +1,15 @@
 from django.db import models
 from django.dispatch import receiver
+from django.contrib.admin import display
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.safestring import mark_safe
 from django.db.models.signals import post_save
+from django.db.models.aggregates import Count
+from django.urls import reverse
 
 
-class UserMixin(object):
-    '''
-    This provides default implementations for the methods that Flask-Login
-    expects user objects to have.
-    '''
-    @property
-    def is_active(self):
-        return True
-
-    @property
-    def is_authenticated(self):
-        return True
-
-    @property
-    def is_anonymous(self):
-        return False
-
-
-class AnonymousUserMixin(object):
-    '''
-    This is the default object for representing an anonymous user.
-    '''
-    @property
-    def is_authenticated(self):
-        return False
-
-    @property
-    def is_active(self):
-        return False
-
-    @property
-    def is_anonymous(self):
-        return True
+DEFAULT_OKOLOTOK_ID = 0
 
 
 # Есть готовые данные в фикстуре. Можно попробовать загрузить.
@@ -48,7 +20,16 @@ class Okolotok(models.Model):
         verbose_name='Номер'
     )
 
+    def count_userprofile(self):
+        return self.userprofile.count()
+
+    def count_report_of_work(self):
+        return self.report_of_work.count()
+
     def __str__(self):
+        if self.pk == 0:
+            return self.name
+
         return f'Околоток {self.name}'
 
     class Meta:
@@ -57,45 +38,62 @@ class Okolotok(models.Model):
 
 
 # Есть готовые данные в фикстуре. Можно попробовать загрузить.
-class ProfileUser(models.Model, UserMixin):
-    user_site = models.OneToOneField(
+class UserProfile(models.Model):
+    user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Пользователь'
-    )
-    active = models.BooleanField(
-        default=True,
-        verbose_name='Доступ к сайту'
+        verbose_name='Пользователь',
+        related_name='userprofile'
     )
     okolotok = models.ForeignKey(
         Okolotok,
-        null=True,
-        on_delete=models.SET_NULL,
+        default=DEFAULT_OKOLOTOK_ID,
+        on_delete=models.PROTECT,
         verbose_name='Околоток',
-        related_name='users'
+        related_name='userprofile'
     )
 
-    def __str__(self):
-        username = self.user_site.username or ''
-        first_name = self.user_site.first_name or ''
-        last_name = self.user_site.last_name or ''
-        okolotok = self.okolotok or ''
+    def count_report_of_work(self):
+        return self.report_of_work.count()
 
-        str_text = f'{username} {first_name} {last_name} {okolotok}'
+    def __str__(self):
+        if self.id is None:
+            return None
+
+        okolotok = self.okolotok or ''
+        str_text = f'{self.user} {okolotok}'
 
         return str.strip(str_text)
-
-    @property
-    def ban_in_bot(self):
-        return not self.active
 
     class Meta:
         verbose_name = 'Данные сотрудника'
         verbose_name_plural = 'Данные сотрудников'
 
 
+class DeviceForWork(models.Model):
+    name = models.CharField(
+        max_length=200,
+        blank=False,
+        verbose_name='Наименование',
+    )
+    description = models.TextField(
+        blank = True,
+        verbose_name='Описание',
+    )
+
+    def count_tech_card(self):
+        return self.tech_card.count()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Инструмент для работы'
+        verbose_name_plural = 'Инструменты для работы'
+
+
 # Есть готовые данные в фикстуре. Можно попробовать загрузить.
-class TPTypeWork(models.Model):
+class TechCard(models.Model):
     code = models.CharField(
         max_length=20,
         blank=False,
@@ -106,17 +104,31 @@ class TPTypeWork(models.Model):
         blank=False,
         verbose_name='Наименование',
     )
+    description = models.TextField(
+        blank = True,
+        verbose_name='Описание',
+    )
     du46 = models.BooleanField(
         default=False,
-        verbose_name='Запись в ДУ46'
+        verbose_name='ДУ46'
+    )
+    order  = models.BooleanField(
+        default=False,
+        verbose_name='журнал распоряжений'
+    )
+    device_for_work = models.ManyToManyField(
+        DeviceForWork,
+        blank=True,
+        verbose_name='Инструмент для работы',
+        related_name='tech_card',
     )
 
     def __str__(self):
         return f'{self.code} {self.name}'
 
     class Meta:
-        verbose_name = 'Техпроцесс'
-        verbose_name_plural = 'Техпроцессы'
+        verbose_name = 'Техкарта'
+        verbose_name_plural = 'Техкарты'
 
 
 # Есть готовые данные в фикстуре. Можно попробовать загрузить.
@@ -132,6 +144,9 @@ class Station(models.Model):
         verbose_name='Сокращение'
     )
 
+    def count_report_of_work(self):
+        return self.report_of_work.count()
+
     def __str__(self):
         return f'{self.name}'
 
@@ -140,36 +155,38 @@ class Station(models.Model):
         verbose_name_plural = 'Станции'
 
 
-#TODO: Добавить набор станций как другая модель в которой есть все варианты
 class ReportOfWork(models.Model):
-    data_start = models.DateTimeField(
+    date_start = models.DateTimeField(
         default=timezone.now,
         null=False,
         verbose_name='Начало'
     )
-    data_end = models.DateTimeField(
+    date_end = models.DateTimeField(
         default=timezone.now,
         null=False,
         verbose_name='Конец'
     )
     station = models.ForeignKey(
         Station,
-        null=True,
-        on_delete=models.SET_NULL, # TODO: возможно лучше другое поведение. (Лучше не удалять эту инфу) Из-за этого изменён вывод str
+        on_delete=models.PROTECT,
         verbose_name='Место (станция)',
-        related_name='records_du46'
+        related_name='report_of_work'
     )
     type_work = models.ManyToManyField(
-        TPTypeWork,
+        TechCard,
         verbose_name='ТехПроцессы',
-        related_name='records_du46'
+        related_name='report_of_work'
     )
-    profile_user = models.ForeignKey(
-        ProfileUser,
-        null=True,
-        on_delete=models.SET_NULL, # TODO: возможно лучше другое поведение. (Лучше не удалять эту инфу) Из-за этого изменён вывод str
+    userprofile = models.ManyToManyField(
+        UserProfile,
         verbose_name='Исполнитель',
-        related_name='records_du46'
+        related_name='report_of_work'
+    )
+    okolotok = models.ForeignKey(
+        Okolotok,
+        on_delete=models.PROTECT,
+        verbose_name='Околоток',
+        related_name='report_of_work'
     )
     note = models.TextField(
         max_length=500,
@@ -184,11 +201,11 @@ class ReportOfWork(models.Model):
     )
 
     def time_delta(self):
-        return self.data_end - self.data_start
+        return self.date_end - self.date_start
 
     def __str__(self):
-        ds = self.data_start.strftime("%d %m %Y")
-        de = self.data_end.strftime("%d %m %Y")
+        ds = self.date_start.strftime("%d %m %Y")
+        de = self.date_end.strftime("%d %m %Y")
         station = str(self.station)
         type_work = ", ".join(self.type_work.all().values_list('code', flat=True))
         
@@ -199,88 +216,12 @@ class ReportOfWork(models.Model):
         verbose_name_plural = 'Записи ТП со временем отключения'
 
 
-class PreReportOfWork(models.Model):
-    data_start = models.DateTimeField(
-        default=None,
-        null=True,
-        blank=True,
-        verbose_name='Начало'
-    )
-    data_end = models.DateTimeField(
-        default=None,
-        null=True,
-        blank=True,
-        verbose_name='Конец'
-    )
-    status_run = models.BooleanField(
-        default=False,
-        verbose_name='Запущена?'
-    )
-    station = models.ForeignKey(
-        Station,
-        default=None,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL, # TODO: возможно лучше другое поведение. (Лучше не удалять эту инфу) Из-за этого изменён вывод str
-        verbose_name='Место (станция)',
-        related_name='pre_records_du46'
-    )
-    type_work = models.ManyToManyField(
-        TPTypeWork,
-        default=None,
-        blank=True,
-        verbose_name='ТехПроцессы',
-        related_name='pre_records_du46'
-    )
-    profile_user = models.ForeignKey(
-        ProfileUser,
-        default=None,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL, # TODO: возможно лучше другое поведение. (Лучше не удалять эту инфу) Из-за этого изменён вывод str
-        verbose_name='Исполнитель',
-        related_name='pre_records_du46'
-    )
-    note = models.TextField(
-        max_length=500,
-        default=None,
-        null=True,
-        blank=True,
-        verbose_name='Примечание'
-    )
-    subdivision = models.CharField(
-        max_length=20,
-        default='СИТ',
-        blank=False,
-        verbose_name='Организация-исполнитель'
-    )
-
-    def time_delta(self):
-        return self.data_end - self.data_start
-
-    def __str__(self):
-        if self.data_start:
-            ds = self.data_start.strftime("%d %m %Y")
-        else:
-            ds = ""
-
-        if self.data_end:
-            de = self.data_end.strftime("%d %m %Y")
-        else:
-            de = ""
-        station = str(self.station)
-        return f'{station} [{ds} - {de}] {self.type_work}' # TODO: А как он отображает manytomany?
-
-    class Meta:
-        verbose_name = 'Подготовительная запись ТП'
-        verbose_name_plural = 'Подготовительные записи ТП'
-
-
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        ProfileUser.objects.create(user_site=instance)
+        UserProfile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profileuser.save()
+    instance.userprofile.save()
