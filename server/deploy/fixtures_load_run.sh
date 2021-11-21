5b25bed87badf3dd229c4695a673d86f5c9c3473
Виноградов Константин <kostya.06.05.1995@gmail.com>
2021-11-21 21:35:17 +0300

    add fixture device. Add prod settings

--- server/deploy/fixtures_load_run.sh
+++ server/deploy/fixtures_load_run.sh
@@ -6,6 +6,7 @@ source ./env/bin/activate
 python manage.py loaddata cms/fixtures/cms.Okolotok.json
 python manage.py loaddata cms/fixtures/cms.Station.json
 python manage.py loaddata cms/fixtures/cms.TechCard.json
+python manage.py loaddata cms/fixtures/cms.DeviceForWork.json
 
 deactivate
 echo "END"
